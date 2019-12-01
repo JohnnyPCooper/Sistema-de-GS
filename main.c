f87acb95c783f25e3fea96ed9a638e1f1699ccb7
gustavorossy <50205229+gustavorossy@users.noreply.github.com>
2019-12-01 13:09:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ FILE *abre_arquivo(); //ABRE UM ARQUIVO SOLICITADO
 int verifica_abertura();
 FILE *proxima_linha(); // PULA O PONTEIRO DO ARQUIVO PARA A PROXIMA LINHA
 Sproduto *le_produto(); // LÊ UM PRODUTO DO TECLADO
+void grava_arquivo();
 void cadastra_produto(); // CADASTRA UM PRODUTO OU ENCAMINHA PARA ALTERA_PRODUTO E GRAVA NO ARQUIVO
 Sproduto *altera_produto(); // ALTERA/DELETA UM PRODUTO NO VETOR DE PRODUTOS
 
@@ -35,6 +36,7 @@ int main(){
 void menu_principal(){ // MOSTRA O MENU PRINCIPAL DO PROGRAMA
     int op;
     while(1){
+        system("cls");
         printf("\n###SISTEMA DE GERENCIAMENTO DE SUPERMERCADO###\n");
         printf("\n1) Cadastrar/alterar/excluir produtos no estoque");
         printf("\n2) Buscar produto no estoque"); //um ou todos os produtos do estoque
@@ -44,8 +46,11 @@ void menu_principal(){ // MOSTRA O MENU PRINCIPAL DO PROGRAMA
         printf("\nOpcao: ");
         scanf("%i", &op);
         switch (op) {
-            case 1:
-
+        case 1:
+            cadastra_produto();
+            printf("\nDigite enter para continuar...\n");
+            getchar(); //limpar o buffer
+            getchar();
         }
 
     }
@@ -54,11 +59,7 @@ void menu_principal(){ // MOSTRA O MENU PRINCIPAL DO PROGRAMA
 FILE *abre_arquivo(char *a, char *b){
     FILE *p;
     p = fopen(a,b);
-    if(verifica_abertura(p) == 0) {
-        printf("Arquivo nao foi aberto. Tente novamente!\n");
-        return NULL;
-    }
-    else return p;
+    return p;
 }
 
 int verifica_abertura(FILE *p){
@@ -79,59 +80,106 @@ FILE *proxima_linha(FILE *p){
     return p;
 }
 
-int numero_de_linhas(FILE *p){
-    char c;
-    int n=0;
-    while(c!=EOF){
-        if(c=='\n') n++;
-    }
-    return n;
-}
-
-Sproduto *le_produto(int aux){
+Sproduto *le_produto(int n){
     Sproduto *aux = (Sproduto*) malloc(sizeof(Sproduto));
-    aux->codigo = aux;
-    printf("\nDigite os novos valos do produto %i:", aux);
-    printf("\nNome: ");
-    scanf("%s\n", aux->nome);
+    aux->codigo = n;
+    printf("Nome: ");
+    scanf("%s", aux->nome);
     printf("Valor da compra: ");
-    scanf("%f", &(produto->val_compra));
+    getchar();
+    scanf("%f", &(aux->val_compra));
     printf("Valor da venda: ");
-    scanf("%f", &(produto->preco));
+    getchar();
+    scanf("%f", &(aux->preco));
     printf("Quantidade: ");
-    scanf("%i", &(produto->quant));
+    getchar();
+    scanf("%i", &(aux->quant));
     return aux;
 }
 
+void grava_arquivo(FILE *p, Sproduto *prod, int n){
+    int i;
+    for(i=0;i<n;i++){
+        fprintf(p, "%i : %s : %.2f : %.2f : %i\n", prod[i].codigo, prod[i].nome, prod[i].val_compra, prod[i].preco, prod[i].quant);
+        printf("Produto gravado:\n%i : %s : %.2f : %.2f : %i\n", prod[i].codigo, prod[i].nome, prod[i].val_compra, prod[i].preco, prod[i].quant);
+    }
+    printf("\nArquivos gravados com sucesso!\n");
+}
+
 void cadastra_produto(){
-    FILE *p = abre_arquivo("produtos.txt", "a+");
-    int aux, n=0, i;
-    printf("Digite o ID do produto: ");
-    scanf("%i", &aux);
-    n=numero_de_linhas(p);
-    if(n>0){
-        Sproduto *produto = (Sproduto*) malloc(sizeof(Sproduto)*n);
-        for(i=0;i<n;i++){ // CARREGA OS PRODUTOS EM UM VETOR DE PRODUTOS
-            fscanf(p, "%i : %s : %f : %f : %i\n", &produto[i].codigo, produto[i].nome, &produto[i].val_compra, &produto[i].preco, &produto[i].quant);
+    int aux, n=1, i, verifica = 0;
+    FILE *p = abre_arquivo("produtos.txt", "r");
+
+    if(verifica_abertura(p)==0){
+        Sproduto *produto;
+        produto = (Sproduto*) malloc(sizeof(Sproduto));
+        FILE *p = abre_arquivo("produtos.txt", "w");
+        printf("Cadastre o primeiro produto: \n");
+        printf("\nDigite o codigo do produto: ");
+        scanf("%i", &aux);
+        produto = le_produto(aux);
+        grava_arquivo(p,produto,1);
+        fclose(p);
+        free(produto);
+    }
+    else{
+        Sproduto *produto = NULL;
+        while(!feof(p)){ // CARREGA OS PRODUTOS EM UM VETOR DE ESTRUTURA ALOCADO DINAMICAMENTE
+            produto = (Sproduto*) realloc(produto, sizeof(Sproduto)*n);
+            fscanf(p,"%i : %s : %f : %f : %i\n", &(produto[n-1].codigo), produto[n-1].nome, &(produto[n-1].val_compra), &(produto[n-1].preco), &(produto[n-1].quant));
+            c = fgetc(p);
+            if(c == EOF) break;
+            else fseek(p, ftell(p)-1, SEEK_SET);
+            n++;
         }
+        fclose(p);
+
+        FILE *p = abre_arquivo("produtos.txt", "w");
+        printf("Digite o codigo do produto: ");
+        scanf("%i", &aux);
         for(i=0;i<n;i++){
-            if(aux==produto[i].codigo){
-                altera_produto(produto, aux);
+            if(produto[i].codigo == aux){
+                produto = altera_produto(produto, &n, aux);
+                grava_arquivo(p,produto,n);
+                fclose(p);
+                free(produto);
+                verifica++;
+                break;
             }
         }
+        if(verifica == 0){
+            n++;
+            produto = (Sproduto*) realloc(produto, sizeof(Sproduto)*n);
+            produto[n-1].codigo = aux;
+            printf("Nome: ");
+            scanf("%s", produto[n-1].nome);
+            printf("Valor da compra: ");
+            getchar();
+            scanf("%f", &(produto[n-1].val_compra));
+            printf("Valor da venda: ");
+            getchar();
+            scanf("%f", &(produto[n-1].preco));
+            printf("Quantidade: ");
+            getchar();
+            scanf("%i", &(produto[n-1].quant));
+            grava_arquivo(p,produto,n);
+            fclose(p);
+            free(produto);
+        }
+
     }
 
 }
 
-Sproduto *altera_produto(Sproduto *produto, int n, int aux){
-    int op, i;
+Sproduto *altera_produto(Sproduto *produto, int *n, int aux){ // ALTERA E EXCLUI
+    int op, i, a = *n, s=0;
     printf("\nEsse ID de produto ja esta cadastrado no sistema. O que deseja fazer?");
     printf("\n1) Alterar\n2) Excluir\nOpcao: ");
     scanf("%i", &op);
     if(op==1){
         Sproduto *entrada = le_produto(aux);
-        Sproduto *saida = (Sproduto*) malloc(sizeof(Sproduto)*n);
-        for(i=0;i<n;i++){
+        Sproduto *saida = (Sproduto*) malloc(sizeof(Sproduto)*a);
+        for(i=0;i<a;i++){
             if(produto[i].codigo != aux){
                 saida[i].codigo = produto[i].codigo;
                 strcpy(saida[i].nome, produto[i].nome);
@@ -147,6 +195,30 @@ Sproduto *altera_produto(Sproduto *produto, int n, int aux){
                 saida[i].quant = entrada->quant;
             }
         }
+        return saida;
+    }
+    else if(op==2){
+        a--;
+        Sproduto *saida = (Sproduto*) malloc(sizeof(Sproduto)*a);
+        for(i=0;i<a+1;i++){
+            if(produto[i].codigo != aux){
+                saida[s].codigo = produto[i].codigo;
+                strcpy(saida[s].nome, produto[i].nome);
+                saida[s].val_compra = produto[i].val_compra;
+                saida[s].preco = produto[i].preco;
+                saida[s].quant = produto[i].quant;
+                s++;
+            }
+
+        }
+        *n = a;
+        return saida;
     }
 
 }
+
+
+
+
+
+/**/
